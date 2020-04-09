1ebaec4f806445f88c0b90ee5eb6ce5bcd695bfb
huuthang09 <57723750+huuthang09@users.noreply.github.com>
2020-04-09 15:10:58 +0700

    Updated Event

--- shared/detailproduct.js
+++ shared/detailproduct.js
@@ -1,20 +1,33 @@
-import React, {useState} from 'react';
-import {Text, TouchableOpacity, Image} from 'react-native';
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import React from "react";
+import { Text, Image} from "react-native";
+import {
+  widthPercentageToDP as wp,
+  
+} from "react-native-responsive-screen";
+import { View } from "native-base";
 
 export default function DetailProduct(props) {
-  const [color, setcolor] = useState({like: false});
-  const toggleLike = () => setcolor({like: !color.like});
+  
   return (
-    <TouchableOpacity
-      style={{flexDirection:'row', marginLeft:wp("7%")}}
-      onPress={toggleLike}>
+    <View
+      style={{ flexDirection: "row", marginLeft: wp("7%") }}>
       <Image
-            style={{width:25, height:25}}
-            source={color.like?props.imagec:props.image}
-            resizeMode="contain"
-          />
-      <Text style={{fontSize:16, fontWeight:'bold', marginLeft:5,  color:color.like?'#e44549':"#bebac2"}}>{props.txt}</Text>
-    </TouchableOpacity>
+        style={{ width: 25, height: 25 }}
+       
+        source={props.image}
+        resizeMode="contain"
+      />
+      <Text
+        style={{
+          fontSize: 16,
+          fontWeight: "bold",
+          marginLeft: 5,
+          
+          color: props.txtColor
+        }}
+      >
+        {props.txt}
+      </Text>
+    </View>
   );
 }
